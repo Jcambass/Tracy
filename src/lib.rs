d343e4a4229eb05caf62f5dfffb5aa5a9a455560
Joel Ambass <Jcambass@users.noreply.github.com>
2023-03-21 10:21:14 +0100

    Add Vec3 Helpers

---
+++ src/lib.rs
@@ -0,0 +1,164 @@
+use std::ops::{Neg, Index, MulAssign, AddAssign, DivAssign, Add, Sub, Mul, Div, IndexMut};
+
+#[derive(Debug)]
+pub struct Vec3 {
+    e: [f64; 3],
+}
+
+impl Vec3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Self { e: [x, y, z] }
+    }
+
+    pub fn x(&self) -> f64 {
+        self.e[0]
+    }
+
+    pub fn y(&self) -> f64 {
+        self.e[1]
+    }
+
+    pub fn z(&self) -> f64 {
+        self.e[2]
+    }
+
+    pub fn length(&self) -> f64 {
+        f64::sqrt(self.length_squared())
+    }
+
+    pub fn length_squared(&self) -> f64 {
+        self[0]*self[0] + self[1]*self[1] + self[2]*self[2]
+    }
+
+    pub fn dot(&self, other: Self) -> f64 {
+        self[0]*other[0] + self[1]*other[1] + self[2]*other[2]
+    }
+
+    pub fn cross(&self, other: Self) -> Self {
+        Self { e: [
+            self[1]*other[2] - self[2]*other[1],
+            self[2]*other[0] - self[0]*other[2],
+            self[0]*other[1] - self[1]*other[0],
+        ]}
+    }
+
+    pub fn unit_vector(&self) -> Self {
+        self / self.length()
+    }
+
+    pub fn print_color(&self) {
+        println!("{} {} {}", (self[0] * 255.999) as i64, (self[1] * 255.999) as i64, (self[2] * 255.999) as i64)
+    }
+}
+
+impl Neg for Vec3 {
+    type Output = Self;
+
+    fn neg(self) -> Self {
+        Self { e: [-self[0], -self[1], -self[2]]}
+    }
+}
+
+impl Index<usize> for Vec3 {
+    type Output = f64;
+    fn index<'a>(&'a self, i: usize) -> &'a f64 {
+        &self.e[i]
+    }
+}
+
+impl IndexMut<usize> for Vec3 {
+    fn index_mut<'a>(&'a mut self, i: usize) -> &'a mut f64 {
+        &mut self.e[i]
+    }
+}
+
+impl AddAssign for Vec3 {
+    fn add_assign(&mut self, other: Self) {
+        *self = Self {
+            e: [
+                self[0] + other[0],
+                self[1] + other[1],
+                self[2] + other[2],
+            ]
+        }
+    }
+}
+
+impl MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, rhs: f64) {
+        self[0] *= rhs;
+        self[1] *= rhs;
+        self[2] *= rhs;
+    }
+}
+
+impl DivAssign<f64> for Vec3 {
+    fn div_assign(&mut self, rhs: f64) {
+        *self *= 1.0/rhs
+    }
+}
+
+impl Add for &Vec3{
+    type Output = Vec3;
+
+    fn add(self, other: Self) -> Vec3 {
+        Vec3 {
+            e: [
+                self[0] + other[0],
+                self[1] + other[1],
+                self[2] + other[2],
+            ]
+        }
+    }
+}
+
+impl Sub for &Vec3{
+    type Output = Vec3;
+
+    fn sub(self, other: Self) -> Vec3 {
+        Vec3 {
+            e: [
+                self[0] - other[0],
+                self[1] - other[1],
+                self[2] - other[2],
+            ]
+        }
+    }
+}
+
+impl Mul for &Vec3{
+    type Output = Vec3;
+
+    fn mul(self, other: Self) -> Vec3 {
+        Vec3 {
+            e: [
+                self[0] * other[0],
+                self[1] * other[1],
+                self[2] * other[2],
+            ]
+        }
+    }
+
+}
+
+impl Mul<f64> for &Vec3{
+    type Output = Vec3;
+
+    fn mul(self, rhs: f64) -> Vec3 {
+        Vec3 {
+            e: [
+                self[0] * rhs,
+                self[1] * rhs,
+                self[2] * rhs,
+            ]
+        }
+    }
+}
+
+impl Div<f64> for &Vec3{
+    type Output = Vec3;
+
+    fn div(self, rhs: f64) -> Vec3 {
+        self * (1.0/rhs)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use tracy::Vec3;
+
 const IMAGE_WIDTH: u32 = 256;
 const IMAGE_HEIGHT: u32 = 256;
 
@@ -5,16 +7,13 @@ fn main() {
     print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
 
     for j in (0..IMAGE_HEIGHT).rev() {
+        eprint!("\rScanlines remaining: {} ", j);
+
         for i in 0..IMAGE_WIDTH {
-            let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
-            let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
-            let b = 0.25;
-
-            let ir = (255.999 * r) as i64;
-            let ig = (255.999 * g) as i64 ;
-            let ib = (255.999 * b) as i64;
-
-            print!("{} {} {}\n", ir, ig, ib);
+            let pixel_color = Vec3::new(i as f64 / (IMAGE_WIDTH - 1) as f64, j as f64 / (IMAGE_HEIGHT - 1) as f64, 0.25);
+            pixel_color.print_color();
         }
     }
+
+    eprint!("\nDone.\n");
 }
