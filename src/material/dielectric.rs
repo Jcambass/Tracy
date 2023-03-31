e8f7c3b65eec1ef567d2aef45a1997a90b078d74
Joel Ambass <Jcambass@users.noreply.github.com>
2023-03-31 12:28:55 +0200

    glass

--- src/material/dielectric.rs
+++ src/material/dielectric.rs
@@ -1,4 +1,4 @@
-use crate::{ray::Ray, hittable::HitRecord, Color, Vec3};
+use crate::{ray::Ray, hittable::HitRecord, Color, Vec3, random_float};
 
 use super::Material;
 
@@ -11,6 +11,13 @@ impl Dielectric {
         Self {
             index_of_refraction,
         }
+    }
+
+    fn reflectance(cosine: f64, ref_idx: f64) -> f64 {
+        // Use Schlick's approximation for reflectance.
+        let mut r0 = (1.0-ref_idx) / (1.0+ref_idx);
+        r0 = r0*r0;
+        return r0 + (1.0-r0)* f64::powi(1.0 - cosine, 5);
     }
 }
 
@@ -27,7 +34,7 @@ impl Material for Dielectric {
         let sin_theta = f64::sqrt(1.0 - cos_theta*cos_theta);
 
         let cannot_refract = refraction_ratio * sin_theta > 1.0;
-        let direction = if cannot_refract {
+        let direction = if cannot_refract || Self::reflectance(cos_theta, refraction_ratio) > random_float() {
             unit_direction.reflect(rec.normal)
         } else {
             unit_direction.refract(rec.normal, refraction_ratio)
